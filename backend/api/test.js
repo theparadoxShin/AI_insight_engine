6d757ee07b9bc1915b46e59efddd8932ca701ef3
theparadoxShin <tedom.tedom7@gmail.com>
2025-06-25 16:51:30 -0400

    test online v10

--- backend/api/test.js
+++
@@ -1,8 +0,0 @@
-import vercel from '@vercel/node';
-const { VercelRequest, VercelResponse } = vercel;
-
-export default function handler(VercelRequest, VercelResponse){
-    const {text = "name test"} = VercelRequest.body;
-    
-    return VercelResponse.status(200).json({message: `Hello ${text}!`,});
-}
